ffde77a854cceba4927f46138e9656fdf4ef177c
S4ufiX21 <40340653+S4ufiX21@users.noreply.github.com>
2018-06-18 00:53:50 +0700

    Add files via upload

---
+++ LITESPAM.sh
@@ -0,0 +1,111 @@
+#!/system/xbin/bash
+clear
+toilet -f slant --gay "LiteSpam"
+sleep 1
+echo "\033[31;1m Tools untuk spam"
+sleep 1
+echo "\033[33;1m Author: S4ufiX21"
+sleep 1
+echo "\033[32;1m Contact Me: https://www.instagram.com/im.brokenheart"
+sleep 1
+echo "\033[34;1m Welcome To My Tools"
+sleep 1
+echo "\033[36;1m Pilih Spam Tools :"
+echo "1. Tokopedia"
+echo "2. Telkomsel"
+echo "3. Matahari Mall"
+echo "4. PHD"
+echo "5. Jd.Id"
+echo "6. Email Bomber"
+echo "0. Keluar"
+echo "\033[30;1m81. Informasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[34;1m"
+figlet "tokopedia"
+php 1.php
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[31;1m"
+figlet "Mthr Mall"
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono12 -F gay "PHD"
+echo "\033[30;1m"
+php 4.php
+fi
+
+if
+[ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+echo "\033[33;1m"
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Email Bomber"
+echo "\033[36;1m"
+python2 6.py
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "LiteSpam"
+echo "\033[31;1mNama tools: LiteSpam"
+sleep 1
+echo "\033[33;1mKarya: S4ufiX21"
+sleep 1
+echo "\033[32;1Author:TAMVANS "
+sleep 1
+echo "\033[36;1Thanks To:ENTAH"
+sleep 1
+echo "\033[34;1mInformasi Lebih Lanjut hubungi www.instagram.com/im.brokenheart"
+echo "\033[31;1m Thanks: "
+echo "\033[36;1m"
+echo "ANE TAMVANS"
+echo "ANE TAMVANS"
+echo "ANE TAMVANS"
+echo "ANE TAMVANS"
+echo "ANE TAMVANS"
+sleep 1
+echo "ANE TAMVANSSSS"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m DADAHHHHHHHHHHHHHH"
+sleep 1
+fi
